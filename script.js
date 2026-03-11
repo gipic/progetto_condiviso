62f8ce4f03eed7ff33b8793a0953f8b25110ae90
gipic <36415807+gipic@users.noreply.github.com>
2026-03-11 15:50:34 +0100

    aggiunta alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+alert("Ciaoo!");
 let casellaNome = document.getElementById("nome");
 let casellaCognome = document.getElementById("cognome");
 let btnRegistra = document.getElementById("btnRegistra");
@@ -7,4 +7,5 @@ btnRegistra.addEventListener("click", function() {
     let nome = casellaNome.value;
     let cognome = casellaCognome.value;
     console.log(nome + " -- " + cognome);
+    alert("persona registrata");
 })
